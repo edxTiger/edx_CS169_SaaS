2685e4aa28b576ec9cf86696248de050f176c494
Vitali <vsomeg@gmail.com>
2014-11-07 09:52:35 -0400

    finish hw0_p1&p2

--- part1.rb
+++ part1.rb
@@ -7,7 +7,8 @@ end
 #Define a method max_2_sum which takes an array of integers as an argument and returns the sum of its two largest elements. For an empty array it should return zero. For an array with just one element, it should return that element. 
 
 def max_2_sum(a)
-
+	a.empty? ? 0 : a.sort.last(2).reduce(:+)
 end
 
-raise "Error [1, 2, 3, 4] must be equal 7" unless max_2_sum [1, 2, 3, 4] != 7
+raise "Error max_2_sum([4]) must be equal 7" unless max_2_sum([4]) == 4
+raise "Error max_2_sum([4, 2, 1, 3]) must be equal 7" unless max_2_sum([4, 2, 1, 3]) == 7
