522819f216779c6b6bd7f1c16fb2466bab459bf6
Vitali <vsomeg@gmail.com>
2014-11-16 00:58:30 -0400

    HW1 p4

--- HW1/ruby-calisthenics/lib/attr_accessor_with_history.rb
+++ HW1/ruby-calisthenics/lib/attr_accessor_with_history.rb
@@ -4,7 +4,18 @@ class Class
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+      def #{attr_name}= (next_value)
+        @#{attr_name}_history = Array.new if @#{attr_name}_history.nil?
+        @#{attr_name}_history.push @#{attr_name}
+        @#{attr_name} = next_value
+      end
     }
+
+    class_eval do
+      def history(name_attr)
+        instance_variable_get("@#{name_attr}_history")
+      end
+    end
   end
 end
+
